4e10e661381dff32996e242a56fe7488eee400a9
Chen Weizhi <164135380+d2d-weizhi@users.noreply.github.com>
2025-04-04 05:40:54 +0000

    Finished running thru Chapter 14 of online tutorial.

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -2,7 +2,7 @@
 import {
   AtSymbolIcon,
   KeyIcon,
-  ExclamationCircleIcon,
+  // ExclamationCircleIcon,
 } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from './button';
